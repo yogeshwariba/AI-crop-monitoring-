c3bad438dfe5f32f509fbf89fb4d2bc27ddf6460
yogeshwariba <yogeshwaribadjadeja@gmail.com>
2026-01-28 16:17:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function loadData() {
+    document.getElementById("temp").innerText = "31 °C";
+    document.getElementById("humidity").innerText = "65 %";
+    document.getElementById("soil").innerText = "Dry (Needs Water)";
+}
